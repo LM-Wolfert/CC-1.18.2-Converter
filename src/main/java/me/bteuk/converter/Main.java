7eb05c933b2c366a89e15f6ba24f08831b6da6f2
ELgamer <65742116+ELgamerETS2@users.noreply.github.com>
2022-12-19 14:24:27 +0100

    Added some stuff from the cubic chunks converter.

---
+++ src/main/java/me/bteuk/converter/Main.java
@@ -0,0 +1,25 @@
+package me.bteuk.converter;
+
+import me.bteuk.converter.cc.CubicChunkReader;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+
+    //This is where the program will start.
+    //This is cli application and thus the arguments will be provided through the args variable.
+
+    /*
+    The format of args is:
+        args[0]: path of input world file folder
+        args[1]: path of output folder
+        args[2]: y min
+        args[3]: y max
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
